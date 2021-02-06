0ae7f7120bc570b09c7f1afe56aa5db867380f13
Kohei SUGI <kohei.sg@gmail.com>
2021-02-06 15:56:18 +0900

    Add sidekiq client settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { namespace: 'walker' }
+  config.redis = { namespace: 'walker' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { namespace: 'walker' }
 end
