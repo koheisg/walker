9af013a641377e0271b1de94ea852a7baa4ea203
Kohei SUGI <kohei.sg@gmail.com>
2021-02-06 15:30:54 +0900

    $ rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_062718) do
+ActiveRecord::Schema.define(version: 2021_02_06_062856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,14 @@ ActiveRecord::Schema.define(version: 2021_02_06_062718) do
   end
 
   create_table "items", force: :cascade do |t|
+    t.bigint "feed_id", null: false
     t.string "title"
     t.string "link"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["feed_id"], name: "index_items_on_feed_id"
   end
 
+  add_foreign_key "items", "feeds"
 end
