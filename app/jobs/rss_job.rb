c40117a4fa8f917eacd5d37c954d985717773819
Kohei SUGI <kohei.sg@gmail.com>
2021-02-06 18:58:27 +0900

    Add description

--- app/jobs/rss_job.rb
+++ app/jobs/rss_job.rb
@@ -8,6 +8,7 @@ class RssJob < ApplicationJob
     RSS::Parser.parse(xml).items.each do |item|
       feed.items.create_or_find_by!(link: item.link) do |i|
         i.title = item.title
+        i.description = item.description
       end
     end
   end
