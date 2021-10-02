c7573397aeec123329427260254701d4609a02e9
Kohei SUGI <kohei.sg@gmail.com>
2021-10-02 14:18:23 +0900

    Add atom

--- app/jobs/rss_job.rb
+++ app/jobs/rss_job.rb
@@ -6,10 +6,18 @@ class RssJob < ApplicationJob
   def perform(feed)
     xml = Net::HTTP.get(URI.parse(feed.url))
     RSS::Parser.parse(xml).items.each do |item|
-      feed.items.create_or_find_by!(link: item.link) do |i|
-        i.title = item.title
-        i.description = item.description
-        i.published_at = item.try(:pubDate)
+      if item.class == RSS::Atom::Feed::Entry
+        feed.items.create_or_find_by!(link: item.link.href) do |i|
+          i.title = item.title.content
+          i.description = item.summary&.content
+          i.published_at = item.published.content
+        end
+      else
+        feed.items.create_or_find_by!(link: item.link) do |i|
+          i.title = item.title
+          i.description = item.description
+          i.published_at = item.try(:pubDate)
+        end
       end
     end
   end
