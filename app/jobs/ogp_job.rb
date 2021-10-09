8d73e1242ee0489c0cd0dfb6c0a1e59b763fd49a
Kohei SUGI <kohei.sg@gmail.com>
2021-10-09 20:33:09 +0900

    Fix

--- app/jobs/ogp_job.rb
+++ app/jobs/ogp_job.rb
@@ -5,16 +5,17 @@ class OgpJob < ApplicationJob
 
   def perform(item)
     ItemOgp.create_or_find_by(item_id: item.id) do |ogp|
-      doc = document_from(item.link)
+      if doc = document_from(item.link)
 
-      ogp.assign_attributes(
-        title: doc.css('meta[property="og:title"]/@content').text,
-        og_type: doc.css('meta[property="og:type"]/@content').text,
-        description: doc.css('meta[property="og:description"]/@content').text,
-        url: doc.css('meta[property="og:url"]/@content').text,
-        site_name: doc.css('meta[property="og:site_name"]/@content').text,
-        image: doc.css('meta[property="og:image"]/@content').text
-      )
+        ogp.assign_attributes(
+          title: doc.css('meta[property="og:title"]/@content').text,
+          og_type: doc.css('meta[property="og:type"]/@content').text,
+          description: doc.css('meta[property="og:description"]/@content').text,
+          url: doc.css('meta[property="og:url"]/@content').text,
+          site_name: doc.css('meta[property="og:site_name"]/@content').text,
+          image: doc.css('meta[property="og:image"]/@content').text
+        )
+      end
     end
   end
 
