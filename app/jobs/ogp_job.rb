2db51e9e52dd4fbeba1c83f1380bb0a622dbd912
Kohei SUGI <kohei.sg@gmail.com>
2021-10-20 17:52:34 +0900

    Update ogp_job.rb

--- app/jobs/ogp_job.rb
+++ app/jobs/ogp_job.rb
@@ -5,7 +5,6 @@ class OgpJob < ApplicationJob
 
   def perform(item)
     ItemOgp.find_or_create_by(item_id: item.id) do |ogp|
-      browser = Ferrum::Browser.new(timeout: 15)
       browser.go_to(item.link)
       ogp.assign_attributes(
         title: browser.at_css('meta[property="og:title"]')&.attribute('content'),
@@ -15,7 +14,13 @@ class OgpJob < ApplicationJob
         site_name: browser.at_css('meta[property="og:site_name"]')&.attribute('content'),
         image: browser.at_css('meta[property="og:image"]')&.attribute('content')
       )
-      browser.quit
     end
+  ensure
+    browser.quit
   end
+  
+  private
+    def browser
+      @browser ||= Ferrum::Browser.new(timeout: 15)
+    end
 end
