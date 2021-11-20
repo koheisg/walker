f3ea64b8f06e9ef991d1b4cb0e2ec3243ba7420c
Kohei SUGI <koheisg@hey.com>
2021-11-20 13:22:48 +0900

    Update restore database logic

--- bin/data_from_production.sh
+++ bin/data_from_production.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-LOCAL_DATABASE_URL=$DATABASE_URL
-PRODUCTION_DATABASE_URL=`heroku config:get DATABASE_URL -a $HEROKU_PRODUCTION_APPLICATION_NAME`
-pg_dump -a -d  $PRODUCTION_DATABASE_URL > hoge.sql
-bundle exec rails db:schema:load
-psql -d $LOCAL_DATABASE_URL/walker_development -f hoge.sql
-rm hoge.sql
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
+heroku pg:pull DATABASE_URL $DATABASE_URL -a $HEROKU_PRODUCTION_APPLICATION_NAME
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create!(email: 'kohei.sg@gmail.com',
-             password: 'test1234',
-             sign_in_count: 1,
-             current_sign_in_at: Time.zone.parse('2021-05-16 14:38:53.63592'),
-             last_sign_in_at: Time.zone.parse('2021-05-16 14:38:53.63592'),
-             current_sign_in_ip: '::1',
-             last_sign_in_ip: '::1',
-             confirmation_token: 'FyDyHr3PK1LKNfNnSqAq',
-             confirmed_at: Time.zone.parse('2021-05-16 14:38:43.222425'),
-             confirmation_sent_at: Time.zone.parse('2021-05-16 14:38:43.222425'),
-             failed_attempts: 0)
-Feed.create!(name: 'はてぶ', url: 'https://b.hatena.ne.jp/koheisg/favorite.rss?key=a2d08e4280c6dc0b17d98321ae230de19d971a74')
