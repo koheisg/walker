476b9a442cb45cd5773af9188c47638e226c0d22
Kohei SUGI <koheisg@hey.com>
2022-06-29 23:10:40 +0900

    Set time zone tokyo

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Walker
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
+    config.time_zone = 'Tokyo'
   end
 end
