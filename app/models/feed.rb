5af3f4981598f1ea1c9a9195c70fcc6577de322e
Kohei SUGI <koheisg@hey.com>
2022-12-31 21:14:12 +0900

    Add has_many

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,6 @@
 class Feed < ApplicationRecord
   has_many :items
+  has_many :feed_groupings, dependent: :destroy
+  has_many :feed_group, through: :feed_groupings
   scope :default, -> { where(default: true) }
 end
