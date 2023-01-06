b848674aef62d71d02d4e36928437409238791e1
Kohei SUGI <koheisg@hey.com>
2023-01-07 01:46:04 +0900

    Update top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,5 @@
 class TopController < ApplicationController
   skip_before_action :authenticate
   def show
-    @date = Date.today
-    @items = Item.includes(:item_ogp, :feed).where(feed: Feed.default).first(24)
   end
 end
